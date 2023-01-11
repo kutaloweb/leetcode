18c8544976cac585a5c276661e59c7efb327d40f
Alexey Kutalo <akutalo@eb.com>
2023-01-11 13:21:49 -0800

    0001_two_sum 3

--- 0001_two_sum.js
+++ 0001_two_sum.js
@@ -1,24 +1,41 @@
 // 1. Two Sum
+
 // Easy
 
 // Given an array of integers nums and an integer target, return
 // indices of the two numbers such that they add up to target.
 
-// Example:
+// Example 1:
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+// Example 2:
 // Input: nums = [3,2,4], target = 6
 // Output: [1,2]
 
 // Time: O(n^2)
 const twoSumBrute = (nums, target) => {
   const result = [];
-  for (let i = 0; i < nums.length - 1; i += 1) {
-    for (let j = i + 1; j < nums.length; j += 1) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
       if (nums[j] === target - nums[i]) {
         result.push(i, j);
       }
     }
   }
   return result;
+}
+
+// Time: O(n^2)
+const twoSumBrute2 = (nums, target) => {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
 }
 
 //Time: O(n)
@@ -38,14 +55,22 @@ const twoSumMap = (nums, target) => {
 const twoSumMap2 = (nums, target) => {
   const map = {};
   for (let i = 0; i < nums.length; i++) {
+    map[nums[i]] = i; // { '3': 0, '2': 1, '4': 2 }
+  }
+  for (let i = 0; i < nums.length; i++) {
     const complement = target - nums[i];
-    if (complement in map) {
-      return [map[complement], i];
+    if (complement in map && map[complement] !== i) {
+      return [i, map[complement]];
     }
-    map[nums[i]] = i;
   }
 }
 
+console.log(twoSumBrute([2,7,11,15], 9));
+console.log(twoSumBrute2([2,7,11,15], 9));
+console.log(twoSumMap([2,7,11,15], 9));
+console.log(twoSumMap2([2, 7, 11, 15], 9));
+
 console.log(twoSumBrute([3, 2, 4], 6));
+console.log(twoSumBrute2([3, 2, 4], 6));
 console.log(twoSumMap([3, 2, 4], 6));
 console.log(twoSumMap2([3, 2, 4], 6));
