3336570c6208b9b298664a7f2a93d94777ccffdd
Alexey Kutalo <akutalo@eb.com>
2023-02-18 11:45:47 -0800

    0002_two_numbers 3

--- 0002_two_numbers.js
+++ 0002_two_numbers.js
@@ -26,50 +26,26 @@ class ListNode {
 const addTwoNumbers = (l1, l2) => {
   let dummyNode = new ListNode(0);
   let current = dummyNode;
-  let pointer1 = l1;
-  let pointer2 = l2;
   let carryOver = 0;
-  while (pointer1 || pointer2) {
-    let sum = carryOver;
-    sum += pointer1 ? pointer1.val : 0;
-    sum += pointer2 ? pointer2.val : 0;
-    carryOver = Math.floor(sum / 10);
-    current.next = new ListNode(sum % 10);
-    current = current.next;
-    pointer1 = pointer1 && pointer1.next;
-    pointer2 = pointer2 && pointer2.next;
-  }
-  if (carryOver > 0) current.next = new ListNode(carryOver);
-  return dummyNode.next;
-}
-
-/**
- * @param {ListNode} l1
- * @param {ListNode} l2
- * @return {ListNode}
- */
-const addTwoNumbers2 = (l1, l2) => {
-  let dummyNode = new ListNode(0);
-  let current = dummyNode;
-  let pointer1 = l1;
-  let pointer2 = l2;
-  let carryOver = 0;
-  while (pointer1 || pointer2) {
-    let x = pointer1 ? pointer1.val : 0;
-    let y = pointer2 ? pointer2.val : 0;
+  while (l1 || l2) {
+    let x = l1 ? l1.val : 0;
+    let y = l2 ? l2.val : 0;
     let sum = x + y + carryOver;
     carryOver = Math.floor(sum / 10);
-    sum = sum % 10;
-    current.next = new ListNode(sum);
-    pointer1 = pointer1 && pointer1.next;
-    pointer2 = pointer2 && pointer2.next;
+    let lastDigit = sum % 10;
+    current.next = new ListNode(lastDigit);
+    l1 = l1 && l1.next;
+    l2 = l2 && l2.next;
     current = current.next;
   }
   if (carryOver > 0) current.next = new ListNode(carryOver);
   return dummyNode.next;
 };
 
-// [2 -> 4 -> 3] + [5 -> 6 -> 4]
+// [2 -> 4 -> 3] +
+// [5 -> 6 -> 4]
+// -------------
+// [7 -> 0 -> 1 + 7]
 let l1 = new ListNode(2);
 l1.next = new ListNode(4);
 l1.next.next = new ListNode(3);
@@ -77,5 +53,4 @@ let l2 = new ListNode(5);
 l2.next = new ListNode(6);
 l2.next.next = new ListNode(4);
 console.log(addTwoNumbers(l1, l2));
-console.log(addTwoNumbers2(l1, l2));
 
